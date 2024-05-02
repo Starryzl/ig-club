6213f3ed50eccda22d3f313b050e8fe7e692946a
Starry <1402454154@qq.com>
2024-05-02 16:36:39 +0800

    更新了pom.xml的结构和内容

--- ig-club-subject/ig-club-infra/src/main/java/com/tencent/subject/infra/basic/utils/DruidEncryptUtil.java
+++ ig-club-subject/ig-club-infra/src/main/java/com/tencent/subject/infra/basic/utils/DruidEncryptUtil.java
@@ -40,7 +40,7 @@ public class DruidEncryptUtil {
     }
 
     public static void main(String[] args) throws Exception {
-        String encrypt = encrypt("root");
+        String encrypt = encrypt("3fe14f9e59c2f695");
         System.out.println("encrypt: "+encrypt);
     }
 
