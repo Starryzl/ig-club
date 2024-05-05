b9c5e7641dc3ec37596a09d236f558b4fae8cb50
Starry <1402454154@qq.com>
2024-05-05 11:38:18 +0800

    nacos动态配置&实现bean动态加载

--- ig-club-oss/src/main/java/com/tencent/oss/config/StorageConfig.java
+++ ig-club-oss/src/main/java/com/tencent/oss/config/StorageConfig.java
@@ -5,18 +5,19 @@ import com.tencent.oss.adapter.StorageAdapter;
 import com.tencent.oss.adapter.AliStorageAdapter;
 import com.tencent.oss.adapter.MinioStorageAdapter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RefreshScope
 public class StorageConfig {
 
-    @NacosValue(value = "${storage.service.type}", autoRefreshed=true)
+    @Value("${storage.service.type}")
     private String storageType;
 
-
-
     @Bean
+    @RefreshScope
     public StorageAdapter storageService(){
         if("minio".equals(storageType)){
             return new MinioStorageAdapter();
