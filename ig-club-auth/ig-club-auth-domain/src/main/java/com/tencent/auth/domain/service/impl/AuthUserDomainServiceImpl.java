3ca00b984911e17c62dc28685cdab7923c571b01
Starry <1402454154@qq.com>
2024-05-21 10:11:03 +0800

    用户密码加密

--- ig-club-auth/ig-club-auth-domain/src/main/java/com/tencent/auth/domain/service/impl/AuthUserDomainServiceImpl.java
+++ ig-club-auth/ig-club-auth-domain/src/main/java/com/tencent/auth/domain/service/impl/AuthUserDomainServiceImpl.java
@@ -1,5 +1,6 @@
 package com.tencent.auth.domain.service.impl;
 
+import cn.dev33.satoken.secure.SaSecureUtil;
 import com.alibaba.fastjson.JSON;
 import com.tencent.auth.common.enums.AuthUserStatusEnum;
 import com.tencent.auth.common.enums.IsDeletedFlagEnum;
@@ -8,10 +9,12 @@ import com.tencent.auth.domain.entity.AuthUserBO;
 import com.tencent.auth.domain.service.AuthUserDomainService;
 import com.tencent.auth.infra.basic.entity.AuthUser;
 import com.tencent.auth.infra.basic.service.AuthUserService;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
 
 @Service
 @Slf4j
@@ -20,9 +23,19 @@ public class AuthUserDomainServiceImpl implements AuthUserDomainService {
     @Resource
     private AuthUserService authUserService;
 
+    private String salt = "ig";
     @Override
+    @SneakyThrows
     public Boolean register(AuthUserBO authUserBO) {
         AuthUser authUser = AuthUserBOConverter.INSTANCE.convertBOToEntity(authUserBO);
+
+/*        HashMap<String, String> keyMap = SaSecureUtil.rsaGenerateKeyPair();
+        String privateKey = keyMap.get("private");
+        String publicKey = keyMap.get("public");
+        authUser.setPassword(SaSecureUtil.rsaEncryptByPublic(publicKey,authUser.getPassword()));
+        SaSecureUtil.rsaDecryptByPrivate(privateKey,authUser.getPassword());*/
+
+        authUser.setPassword(SaSecureUtil.md5BySalt(authUser.getPassword(),salt));
         authUser.setStatus(AuthUserStatusEnum.OPEN.getCode());
         authUser.setIsDeleted(IsDeletedFlagEnum.UN_DELETED.getCode());
         if (log.isInfoEnabled()) {
