5c1a3658ace52d44c7c2dc31a3881c7ce2c332ed
Starry <1402454154@qq.com>
2024-05-24 12:48:06 +0800

    公众号验签开发

---
+++ ig-club-wx/src/main/java/com/tencent/wx/WxApplication.java
@@ -0,0 +1,18 @@
+package com.tencent.wx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * 微信服务启动类
+ */
+
+@SpringBootApplication
+@ComponentScan("com.tencent")
+public class WxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxApplication.class,args);
+    }
+}
---
+++ ig-club-wx/src/main/java/com/tencent/wx/controller/CallBackController.java
@@ -0,0 +1,37 @@
+package com.tencent.wx.controller;
+
+import com.tencent.wx.utils.SHA1;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class CallBackController {
+
+    private static final String token = "afasfsdafa";
+
+    @RequestMapping("/test")
+    public String test(){
+        return "hello world";
+    }
+
+    /**
+     * 回调信息检验
+     */
+    @GetMapping("callback")
+    public String callback(@RequestParam("signature") String signature,
+                           @RequestParam("timestamp") String timestamp,
+                           @RequestParam("nonce") String nonce,
+                           @RequestParam("echostr") String echostr) {
+        log.info("get验签请求参数：signature:{}，timestamp:{}，nonce:{}，echostr:{}",
+                signature, timestamp, nonce, echostr);
+        String shaStr = SHA1.getSHA1(token, timestamp, nonce, "");
+        if (signature.equals(shaStr)) {
+            return echostr;
+        }
+        return "unknown";
+    }
+}
