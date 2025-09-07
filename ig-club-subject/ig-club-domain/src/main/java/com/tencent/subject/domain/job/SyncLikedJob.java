235ed5980a9494c59d03f94cde38090531af972d
Starry <1402454154@qq.com>
2025-09-07 17:23:37 +0800

    xxl-job定时任务集成

---
+++ ig-club-subject/ig-club-domain/src/main/java/com/tencent/subject/domain/job/SyncLikedJob.java
@@ -0,0 +1,37 @@
+package com.tencent.subject.domain.job;
+
+import com.xxl.job.core.context.XxlJobHelper;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 同步点赞数据
+ */
+@Component
+@Slf4j
+public class SyncLikedJob {
+    /**
+     * 同步点赞数据任务
+     */
+    @XxlJob("syncLikedJobHandler")
+    public void syncLikedJobHandler() throws Exception {
+        XxlJobHelper.log("syncLikedJobHandler.start");
+        try {
+            log.info("123456");
+        } catch (Exception e) {
+            XxlJobHelper.log("syncLikedJobHandler.error" + e.getMessage());
+        }
+    }
+}
